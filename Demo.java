89a2412735f1ceb071ae6004bb86821ec2cb5487
iwei20 <44007607+ivanwei20@users.noreply.github.com>
2020-10-21 16:08:36 -0400

    Move methods from ArrayThings.java to Demo.java

--- Demo.java
+++ Demo.java
@@ -1,16 +1,73 @@
 public class Demo {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         if(args.length > 0) {
             printLoop(Integer.parseInt(args[0]));
         } else {
             printLoop(5);
         }
 
+        System.out.println(arrayDeepToString(create2DArrayRandomized(6, 4, 4)));
     }
 
-    public static void printLoop(int n){
-        for(int i = 0; i < n; ++i){
-            for(int j = 0; j < n - i; ++j){
+    public static String arrayDeepToString(int[][] arr) {
+        if(arr.length == 0) {
+            return "{}";
+        }
+
+        String result = "{";
+        for(int i = 0; i < arr.length - 1; ++i) {
+            result += arrToString(arr[i]) + ", ";
+        }
+        result += arrToString(arr[arr.length - 1]) + "}";
+
+        return result;
+    }
+
+    public static String arrToString(int[] arr) {
+        if(arr.length == 0) {
+            return "{}";
+        }
+
+        String result = "{";
+        for(int i = 0; i < arr.length - 1; ++i) {
+          result += arr[i] + ", ";
+        }
+        result += arr[arr.length - 1] + "}";
+
+        return result;
+    }
+
+    public static int[][] create2DArray(int rows, int cols, int maxValue) {
+        int[][] result = new int[rows][cols];
+
+        for(int i = 0; i < rows; ++i) {
+            for(int j = 0; j < cols; ++j) {
+                result[i][j] = (int) Math.round(maxValue * Math.random());
+            }
+        }
+
+        return result;
+    }
+
+    public static int[][] create2DArrayRandomized(int rows, int cols, int maxValue) {
+        int[][] result = new int[rows][];
+
+        for(int i = 0; i < rows; ++i) {
+
+            int[] randWidthRow = new int[(int) Math.round(maxValue * Math.random())];
+            for(int j = 0; j < randWidthRow.length; ++j) {
+                randWidthRow[j] = (int) Math.round(maxValue * Math.random());
+            }
+            result[i] = randWidthRow;
+
+        }
+        
+        return result;
+    }
+
+    public static void printLoop(int n) {
+        for(int i = 0; i < n; ++i) {
+            for(int j = 0; j < n - i; ++j) {
                 System.out.print(i + 1);
             }
             System.out.println();
