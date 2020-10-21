10361c17f98cc9857c5f204e16b84f3724ac886e
iwei20 <44007607+ivanwei20@users.noreply.github.com>
2020-10-21 15:59:57 -0400

    Format ArrayThings.java

--- ArrayThings.java
+++ ArrayThings.java
@@ -1,55 +1,62 @@
 public class ArrayThings {
 
     public static void main(String[] args) {
-        int[][] twoDimensions = {
-            {0, 1, 2},
-            {0, 1, 2, 3, 4},
-            {0, 1, 2, 3},
-            {0},
-            {0, 1}
-        };
-        System.out.println(arrayDeepToString(twoDimensions));
+        System.out.println(arrayDeepToString(create2DArrayRandomized(6, 4, 4)));
     }
 
     public static String arrayDeepToString(int[][] arr) {
-        if(arr.length == 0) return "{}";
+        if(arr.length == 0) {
+            return "{}";
+        }
+
         String result = "{";
         for(int i = 0; i < arr.length - 1; ++i) {
             result += arrToString(arr[i]) + ", ";
         }
         result += arrToString(arr[arr.length - 1]) + "}";
+
         return result;
     }
 
-    public static String arrToString(int[] arr){
-        if(arr.length == 0) return "{}";
+    public static String arrToString(int[] arr) {
+        if(arr.length == 0) {
+            return "{}";
+        }
+
         String result = "{";
         for(int i = 0; i < arr.length - 1; ++i) {
           result += arr[i] + ", ";
         }
         result += arr[arr.length - 1] + "}";
+
         return result;
     }
 
     public static int[][] create2DArray(int rows, int cols, int maxValue) {
         int[][] result = new int[rows][cols];
+
         for(int i = 0; i < rows; ++i) {
             for(int j = 0; j < cols; ++j) {
                 result[i][j] = (int) Math.round(maxValue * Math.random());
             }
         }
+
         return result;
     }
 
     public static int[][] create2DArrayRandomized(int rows, int cols, int maxValue) {
         int[][] result = new int[rows][];
+
         for(int i = 0; i < rows; ++i) {
+
             int[] randWidthRow = new int[(int) Math.round(maxValue * Math.random())];
             for(int j = 0; j < randWidthRow.length; ++j) {
                 randWidthRow[j] = (int) Math.round(maxValue * Math.random());
             }
             result[i] = randWidthRow;
+
         }
+        
         return result;
     }
 }
